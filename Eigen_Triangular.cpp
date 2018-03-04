88bc02bfa19c7fc8a9b3daee268dda91c3f7d08f
adarsh1001 <adarshpalsingh1996@gmail.com>
2018-03-05 02:45:54 +0530

    Expanded Addition for Eigen case

--- Eigen_Triangular.cpp
+++ Eigen_Triangular.cpp
@@ -28,9 +28,9 @@ public:
     }
   }
 
-  SymMat<T> operator + (const SymMat<T> &Obj)   //Overload +
+  SymMat<T> operator + (const SymMat<T> &Obj)   //Overload + for Sym
   {
-    SymMat S;
+    SymMat<T> S;
     S.rows = rows;
     S.cols = cols;
     for(int i=0; i<V.size(); i++)
@@ -38,6 +38,31 @@ public:
       S.V.push_back(V[i]+Obj.V[i]);
     }
     return S;
+  }
+
+  template <typename D>
+  SymMat<T> operator + (const Eigen::MatrixBase<D> &Obj)   //Overload + for Eigen
+  {
+    //SymMat<T> S_1(Obj);
+    SymMat<T> S_2;
+    S_2.rows = rows;
+    S_2.cols = cols;
+    /*
+    for(int i=0; i<V.size(); i++)
+    {
+      S_2.V.push_back(V[i]+S_1.V[i]);
+    }
+    */
+    int k=0;
+    for(int i=0; i<cols; i++)
+    {
+      for(int j=i; j<rows; j++)
+      {
+        S_2.V.push_back(V[k]+Obj(i,j));
+        k++;
+      }
+    }
+    return S_2;
   }
 
   T Access(int r, int c)      //Access specific Position
@@ -78,6 +103,8 @@ int main()
   M1 << 1,2,3,4,5,6,7,8,9;
   Eigen::MatrixXi M2(3,3);
   M2 << 1,1,1,1,1,1,1,1,1;
+  Eigen::MatrixXi M3(3,3);
+  M3 << 1,1,1,1,1,1,1,1,1;
   SymMat<int> S1(M1);
   SymMat<int> S2(M2);
   SymMat<int> S3;
@@ -86,6 +113,9 @@ int main()
   cout<<"\nSymMat 2: ";
   cout<<S2;
   S3 = S1 + S2;
+  cout<<"\nSymMat 3: ";
+  cout<<S3;
+  S3 = S1 + M3;
   cout<<"\nSymMat 3: ";
   cout<<S3;
   cout<<endl;
